30efc75fe305c1839ce6f942be4b385a1e87698b
abot8639 <abot8639@gmail.com>
2025-04-25 20:36:17 +0300

    add timer

---
+++ lib/controller/controller.Getx.dart
@@ -0,0 +1,14 @@
+import 'package:get/get.dart';
+
+class imagecontroller extends GetxController {
+  RxInt ImageIndex = 0.obs;
+
+  List ImageList = [
+    'assets/images/1.jpg',
+    'assets/images/2.png',
+    'assets/images/3.jpg',
+    'assets/images/4.jpg',
+    'assets/images/5.jpg',
+    'assets/images/6.jpg',
+  ];
+}
--- lib/view/page/HomePage.dart
+++ lib/view/page/HomePage.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:my_clock/controller/controller.Getx.dart';
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedContainer(
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(20),
+    final controller = Get.put(imagecontroller());
+    return Obx(
+      () => AnimatedContainer(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+              controller.ImageList[controller.ImageIndex.value],
+            ),
+            fit: BoxFit.cover,
+          ),
+          color: Colors.transparent,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        duration: Duration(milliseconds: 300),
+
+        child: Scaffold(backgroundColor: Colors.transparent),
       ),
-      duration: Duration(milliseconds: 300),
-      child: Scaffold(),
     );
   }
 }
